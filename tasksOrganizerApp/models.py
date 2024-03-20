f3a8a51e4d7426d2030bdec7024aa834400c1173
Giovanni <giovannimaerean@gmail.com>
2024-03-20 19:39:08 +0100

    create subject model

--- tasksOrganizerApp/models.py
+++ tasksOrganizerApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Course(models.Model):
@@ -9,4 +10,18 @@ class Course(models.Model):
     beginningDate = models.DateField()
     finalDate = models.DateField()
 
+    def __str__(self):
+        return f"{self.title} {self.year}"
 
+
+class Subject(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    teacher = models.CharField(max_length=50)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+    
