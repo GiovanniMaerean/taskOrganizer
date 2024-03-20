0ec40366b152fbe907c5a6a986fb797ca235ebd9
Giovanni <giovannimaerean@gmail.com>
2024-03-20 19:31:21 +0100

    create course model

--- tasksOrganizerApp/models.py
+++ tasksOrganizerApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+    year = models.IntegerField()
+    beginningDate = models.DateField()
+    finalDate = models.DateField()
+
+
